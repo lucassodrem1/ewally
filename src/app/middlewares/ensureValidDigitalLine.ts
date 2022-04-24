f395573e4b12895b49deb57b788e7668469722b0
lucassodrem1 <lucassodrem@gmail.com>
2022-04-24 19:14:15 -0300

    Add async error handler.

--- src/app/middlewares/ensureValidDigitalLine.ts
+++ src/app/middlewares/ensureValidDigitalLine.ts
@@ -1,33 +1,29 @@
 import AppError from '@common/errors/AppError';
-import catchAsync from '@common/errors/catchAsync';
 import DigitalLine from '@common/utils/DigitalLine';
 import { NextFunction, Request, Response } from 'express';
 
 class EnsureValidDigitalLine {
-  execute = catchAsync(
-    async (req: Request, _res: Response, next: NextFunction) => {
-      const { digitalLine } = req.params;
+  async execute(req: Request, _res: Response, next: NextFunction) {
+    const { digitalLine } = req.params;
 
-      if (Number.isNaN(Number(digitalLine)))
-        throw new AppError('Only numbers are allowed on the digital line.');
+    if (Number.isNaN(Number(digitalLine)))
+      throw new AppError('Only numbers are allowed on the digital line.');
 
-      if (digitalLine.length !== 47)
-        throw new AppError('Invalid digital line.');
+    if (digitalLine.length !== 47) throw new AppError('Invalid digital line.');
 
-      const { field1, field2, field3 } = DigitalLine.getFields(digitalLine);
+    const { field1, field2, field3 } = DigitalLine.getFields(digitalLine);
 
-      const hasValidVerifyingDigit = DigitalLine.validVerifyingDigit({
-        field1,
-        field2,
-        field3,
-      });
+    const hasValidVerifyingDigit = DigitalLine.validVerifyingDigit({
+      field1,
+      field2,
+      field3,
+    });
 
-      if (!hasValidVerifyingDigit)
-        throw new AppError('Digital line has an invalid verifying digit.');
+    if (!hasValidVerifyingDigit)
+      throw new AppError('Digital line has an invalid verifying digit.');
 
-      return next();
-    },
-  );
+    return next();
+  }
 }
 
 export default new EnsureValidDigitalLine();
--- src/common/errors/catchAsync.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export default (fn: any) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next);
-  };
